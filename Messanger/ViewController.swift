14320f7238a81532ec40678d4ea2e3c166836a8b
Filip Zobic <filipzobic@Filips-Mac-mini.local>
2022-03-05 00:32:09 +0100

    Initial commit

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Filip Zobic on 5.3.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
