6823eaa2302c9b3e24cf8c1853facbd4606e7f07
Filip Zobic <filipzobic@Filips-Mac-mini.local>
2022-03-07 16:26:37 +0100

    Create google OAuth2

--- Messanger/Resources/AppDelegate.swift
+++ Messanger/Resources/AppDelegate.swift
@@ -8,9 +8,11 @@
 import UIKit
 import FBSDKLoginKit
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
 func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -20,6 +22,8 @@ func application(
         application,
         didFinishLaunchingWithOptions: launchOptions
     )
+//    GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
+//    GIDSignIn.sharedInstance()?.delegate = self
 
     return true
 }
@@ -28,13 +32,29 @@ func application(
     _ app: UIApplication,
     open url: URL,
     options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-) -> Bool {
-    ApplicationDelegate.shared.application(
-        app,
-        open: url,
-        sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-        annotation: options[UIApplication.OpenURLOptionsKey.annotation]
-    )
-}
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+        return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        guard error == nil else {
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        
+        
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectNotificationWith user: GIDGoogleUser!, withError error: Error!) {
+        
+    }
 }
 
