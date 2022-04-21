791961375363bc584abf8899c1478f5f42738bae
Filip Zobic <filipzobic@Filips-Mac-mini.local>
2022-04-21 15:34:28 +0200

    Create video messages & tidy up user search

--- Messanger/Controllers/NewConversationViewController.swift
+++ Messanger/Controllers/NewConversationViewController.swift
@@ -10,10 +10,10 @@ import JGProgressHUD
 
 class NewConversationViewController: UIViewController {
     
-    public var completion: (([String: String]) -> (Void))?
+    public var completion: ((SearchResult) -> (Void))?
     
     private var users = [[String: String]]()
-    private var results = [[String: String]]()
+    private var results = [SearchResult]()
     private var hasFetched = false
 
     private let spinner = JGProgressHUD(style: .dark)
@@ -27,7 +27,7 @@ class NewConversationViewController: UIViewController {
     private let tableView: UITableView = {
         let table = UITableView()
         table.isHidden = true
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.register(NewConversationCell.self, forCellReuseIdentifier: NewConversationCell.identifier)
         return table
     }()
     
@@ -76,8 +76,9 @@ extension NewConversationViewController: UITableViewDelegate, UITableViewDataSou
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = results[indexPath.row]["name"]
+        let model = results[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewConversationCell.identifier, for: indexPath) as! NewConversationCell
+        cell.configure(with: model)
         return cell
     }
     
@@ -129,18 +130,29 @@ extension NewConversationViewController: UISearchBarDelegate {
     }
     
     func filterUsers(with term: String) {
-        guard hasFetched else {
+        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String,  hasFetched else {
             return
         }
         
+        let safeEmail = DatabaseManager.safeEmail(currentUserEmail)
+        
         self.spinner.dismiss()
         
-        var results: [[String: String]] = self.users.filter({
+        let results: [SearchResult] = self.users.filter({
+            guard let email = $0["email"], email != safeEmail else {
+                      return false;
+                  }
+            
             guard let name = $0["name"]?.lowercased() else {
                 return false
             }
             
             return name.hasPrefix(term.lowercased())
+        }).compactMap({
+            guard let email = $0["email"], let name = $0["name"] else {
+                      return nil;
+                  }
+            return SearchResult(name: name, email: email)
         })
         self.results = results
         updateUI()
@@ -156,4 +168,13 @@ extension NewConversationViewController: UISearchBarDelegate {
             self.tableView.reloadData()
         }
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+}
+
+struct SearchResult {
+    let name: String
+    let email: String
 }
